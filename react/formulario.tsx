38f641facf38f1d1445ec0e36e6c356b31dd0a7d
Steffany Martins Soares <steffany@zee-dog.com>
2021-08-29 00:35:56 -0300

    changes in the code call items

--- react/formulario.tsx
+++ react/formulario.tsx
@@ -1,126 +1,94 @@
-import './formulario.css';
-import axios from 'axios';
-import * as React from 'react';
+import React from 'react';
 
-var testeData = Date()
+import { useEffect, useState } from 'react';
 
-var cad = 'cadastrado'
-interface IForm {
-  id: string;
-  name: string;
-  empresa: string;
-  number: string;
-  date: string;
-  leds: string;
-  recado: string;
-}
+import api from './api';
 
-class formulario extends React.Component<any, any> {
-  state:IForm
-  constructor(props:IForm) {
-    super(props);
-    this.state = { id: '', name: '', empresa: '', number: '' , date: '', leds:'', recado:''};
-
-    this.handleChangeI = this.handleChangeI.bind(this);
-    this.handleChangeN = this.handleChangeN.bind(this);
-    this.handleChangeM = this.handleChangeM.bind(this);
-    this.handleChangeU = this.handleChangeU.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChangeR = this.handleChangeR.bind(this);
-
-  }
-  
-
-  handleChangeI(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ id: event.target.value });
-  }
-  handleChangeN(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ name: event.target.value });
-  }
-  handleChangeM(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ empresa: event.target.value });
-  }
-  handleChangeU(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ number: event.target.value });
-  }
-  handleChangeR(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ recado: event.target.value });
-  }
-  
+// import 'vtex-tachyons'
 
 
-  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const name = this.state.name;
-    const id = this.state.id;
-    const number = this.state.number;
-    const empresa = this.state.empresa;
-    const date = testeData;
-    const leds = cad;
-    const recado = this.state.recado;
-    axios({
-      method: 'put',
-      url: 'https://lqbi4t23pj.execute-api.us-west-2.amazonaws.com/items',
-      data: {
-        id: id,
-        name: name,
-        number: number,
-        empresa: empresa,
-        date: date,
-        leds: leds,
-        recado : recado
-      }
-    });
-    let content = document.getElementById('area');
+interface LeadsList {}
 
-    let carregando = `<p>CARREGANDO...</p>`
-
-    let pronto = `<p class="cadastrado">CADASTRADO</p>`
-
-    content!.innerHTML = carregando
-
-
-    setTimeout(() => {
-        content!.innerHTML = pronto
-    }, 1000)
-  }
-
-  render() {
-    return (
-      <section className="contact-area">
-      <div id ="area">
-        <fieldset>
-        <h3>Cadastro de clientes</h3>
-        <form onSubmit={this.handleSubmit} id="formulario">
-
-          <label>
-            E-mail:
-            <input type="text" value={this.state.id} onChange={this.handleChangeI} />
-          </label>
-          <label>
-            Nome:
-            <input type="text" value={this.state.name} onChange={this.handleChangeN} />
-          </label>
-          <label>
-            Telefone:
-            <input type="number" value={this.state.number} onChange={this.handleChangeU} />
-          </label>
-          <label>
-            Empresa:
-            <input type="text" value={this.state.empresa} onChange={this.handleChangeM} />
-          </label>
-          <label>
-            Recado:
-            <input type="recado" value={this.state.recado} onChange={this.handleChangeR} />
-          </label>
-          <input type="submit" value="Enviar" />
-        </form>
-        </fieldset>
-      </div>
-      </section>
-    );
-  }
+interface Leads{
+  leds?: string
+  empresa: string
+  id: string
+  name: string
+  number: string
+  recado?: string
 }
 
 
-export default formulario
+const LeadsList: StorefrontFunctionComponent<LeadsList> = ({}) => {
+
+
+  const [leads, setLeads] = useState<Leads[]>([]);
+
+ async function getLeads(){
+  const response = await api.get('/items')
+
+  setLeads(response.data.Items)
+  
+  console.log(response.data.Items)
+
+
+
+ 
+  }
+
+  useEffect( () => {
+    getLeads();   
+  });
+
+
+  return (
+      <div className="w-80 center pt4">
+        <div className="">
+          <div className="">
+          <div>
+            <h1 className="f1">Lista de Leads</h1>
+          </div>
+
+          <div className="">
+            <table className="f6 mb5 w-100 center">
+              <thead className="">
+                <tr className="stripe-dark">
+                <th className="bg-white fw6 tl bb b--black-20 pt2">empresa</th>
+                <th className="bg-white fw6 tl bb b--black-20 pt2">Nome</th>
+                <th className="bg-white fw6 tl bb b--black-20 pt2">E-mail</th>
+                <th className="bg-white fw6 tl bb b--black-20 pt2">Telefone</th>
+                <th className="bg-white fw6 tl bb b--black-20 pt2">Data de Cadastro</th>
+                <th className="bg-white fw6 tl bb b--black-20 pt2">recado</th>
+                </tr>
+              </thead>
+
+              <tbody className="lh-copy">
+              {leads.length > 0 ?
+                leads.map(lead => (
+                  <tr className="stripe-dark bb b--black-20"  key={lead.id}>
+                    <td className="tl bb b--black-20 pt2">{lead.id}</td>
+                    <td className="tl bb b--black-20 pt2">{lead.name}</td>
+                    <td className="tl bb b--black-20 pt2">{lead.number}</td>
+                    <td className="tl bb b--black-20 pt2">{lead.empresa}</td>
+                    <td className="tl bb b--black-20 pt2">{lead.leds === '' ? '-': lead.leds}</td>
+                    <td className="tl bb b--black-20 pt2">{lead.recado}</td>
+                  </tr>
+                )) : ''}
+              </tbody>
+            </table>
+          </div>
+          </div>
+        </div>
+
+    </div>
+  )}
+
+
+LeadsList.schema = {
+  title: 'editor.leadslist.title',
+  description: 'editor.leadslist.description',
+  type: 'object',
+  properties: {},
+}
+
+export default LeadsList;
